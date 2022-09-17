624c1004e76117f292617f400eb9f93b3c839748
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-09-17 18:02:27 -0300

    feat: add backend routes

---
+++ server/src/server.ts
@@ -0,0 +1,98 @@
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+import { convertHourStrToMin } from "./utils/convert-hour-str-to-min";
+import { convertMinToHoursStr } from "./utils/convert-min-to-hour-str";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
+
+app.get("/games", async (req, res) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
+  return res.json(games);
+});
+
+app.get("/games/:id/ads", async (req, res) => {
+  const gameId = req.params.id;
+
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true,
+    },
+    where: {
+      gameId: gameId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return res.json(
+    ads.map((ad) => {
+      return {
+        ...ad,
+        weekDays: ad.weekDays.split(","),
+        hourStart: convertMinToHoursStr(ad.hourStart),
+        hourEnd: convertMinToHoursStr(ad.hourEnd),
+      };
+    })
+  );
+});
+
+app.get("/ads/:id/discord", async (req, res) => {
+  const adId = req.params.id;
+
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adId,
+    },
+  });
+
+  return res.json({
+    discord: ad.discord,
+  });
+});
+
+app.post("/games/:id/ads", async (req, res) => {
+  const gameId = req.params.id;
+  const body = req.body;
+
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(","),
+      hourStart: convertHourStrToMin(body.hourStart),
+      hourEnd: convertHourStrToMin(body.hourEnd),
+      useVoiceChannel: body.useVoiceChannel,
+    },
+  });
+
+  return res.status(201).json(ad);
+});
+
+app.listen(3333);

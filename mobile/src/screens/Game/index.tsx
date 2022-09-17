c825c69437d3ab76901f6eacf82dfb18a4804e5e
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-09-17 20:04:57 -0300

    feat: create game screens

---
+++ mobile/src/screens/Game/index.tsx
@@ -0,0 +1,95 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useRoute, useNavigation } from "@react-navigation/native";
+import { View, TouchableOpacity, Image, FlatList, Text } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+
+import { GameParams } from "../../@types/navigation";
+
+import { Background } from "../../components/Background";
+import { Heading } from "../../components/Heading";
+import { DuoCard, DuoCardProps } from "../../components/DuoCard";
+import { DuoMatch } from "../../components/DuoMatch";
+
+import { styles } from "./styles";
+import { THEME } from "../../theme";
+
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { useEffect, useState } from "react";
+
+export function Game() {
+  const route = useRoute();
+  const navigation = useNavigation();
+  const game = route.params as GameParams;
+
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
+  const [discordDuoSelected, setDiscordDuoSelected] = useState<string>("");
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
+  async function getDiscordUser(adsId: string) {
+    fetch(`http://192.168.18.5:3333/ads/${adsId}/discord`)
+      .then((response) => response.json())
+      .then((data) => setDiscordDuoSelected(data.discord));
+  }
+
+  useEffect(() => {
+    fetch(`http://192.168.18.5:3333/games/${game.id}/ads`)
+      .then((response) => response.json())
+      .then((data) => setDuos(data));
+  }, []);
+
+  return (
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+
+          <Image source={logoImg} style={styles.logo} />
+
+          <View style={styles.right} />
+        </View>
+
+        <Image
+          style={styles.cover}
+          source={{ uri: game.bannerUrl }}
+          resizeMode="cover"
+        />
+
+        <Heading title={game.title} subtitle={"Conecte-se e comece a jogar!"} />
+
+        <FlatList
+          data={duos}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <DuoCard data={item} onConnect={() => getDiscordUser(item.id)} />
+          )}
+          horizontal
+          style={styles.containerList}
+          contentContainerStyle={
+            duos.length > 0 ? styles.contentList : styles.emptyListContent
+          }
+          showsHorizontalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              Não há anúncios publicados ainda.
+            </Text>
+          )}
+        />
+
+        <DuoMatch
+          visible={discordDuoSelected.length > 0}
+          discord={discordDuoSelected}
+          onClose={() => setDiscordDuoSelected("")}
+        />
+      </SafeAreaView>
+    </Background>
+  );
+}

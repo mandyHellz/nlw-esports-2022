7136c5d895994b488f069e2ecb89ad72db5196eb
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-10-06 13:02:07 -0300

    chore: add new configs to tailwind

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -15,6 +15,10 @@ module.exports = {
         'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       },
     },
+    minWidth: {
+      xs: '375px',
+      sm: '640px',
+    },
   },
   plugins: [],
 }

cf1e070877fee9ce8d044195453ccd4ac4319d0e
Amanda Nunes <carvalhonns.amanda@gmail.com>
2022-10-06 13:03:29 -0300

    feat: added keen slider to gameBanner div component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,9 +2,53 @@ import { useEffect, useState } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
 import GameBanner from "./components/GameBanner";
 import CreateAdBanner from "./components/CreateAdBanner";
+import CreateAdModal from "./components/CreateAdModal";
+import { useKeenSlider } from "keen-slider/react";
 import logo from "/Logo.svg";
 import "./styles/main.css";
-import CreateAdModal from "./components/CreateAdModal";
+import "keen-slider/keen-slider.min.css";
+
+const sliderConfigs = {
+  loop: true,
+  initial: 0,
+  mode: "free-snap",
+  slides: {
+    perView: 1,
+    spacing: 10,
+  },
+  breakpoints: {
+    "(min-width: 400px)": {
+      slides: {
+        perView: 2,
+        spacing: 20,
+      },
+    },
+    "(min-width: 640px)": {
+      slides: {
+        perView: 3,
+        spacing: 40,
+      },
+    },
+    "(min-width: 768px)": {
+      slides: {
+        perView: 4,
+        spacing: 40,
+      },
+    },
+    "(min-width: 1024px)": {
+      slides: {
+        perView: 5,
+        spacing: 40,
+      },
+    },
+    "(min-width: 1280px)": {
+      slides: {
+        perView: 6,
+        spacing: 40,
+      },
+    },
+  },
+};
 
 interface Game {
   id: string;
@@ -16,7 +60,9 @@ interface Game {
 }
 
 function App() {
+  const [sliderOptions, setSliderOptions] = useState({});
   const [games, setGames] = useState<Game[]>([]);
+  const [sliderRef] = useKeenSlider(sliderOptions);
 
   useEffect(() => {
     fetch("http://localhost:3333/games")
@@ -26,11 +72,15 @@ function App() {
       });
   }, []);
 
-  return (
-    <div className="max-w-[1244px] mx-auto flex flex-col items-center my-20">
-      <img src={logo} alt="logo" />
+  useEffect(() => {
+    setSliderOptions(sliderConfigs);
+  }, [games]);
 
-      <h1 className="text-6xl text-white font-black mt-20">
+  return (
+    <div className="min-w-xs max-w-[1244px] mx-auto px-2 flex flex-col items-center my-12 sm:my-20">
+      <img src={logo} alt="logo" className="w-1/2 sm:w-fit" />
+
+      <h1 className="text-5xl sm:text-6xl text-white text-center sm:text-left font-black mt-12 sm:mt-20">
         Seu{" "}
         <span className="bg-nlw-gradient text-transparent bg-clip-text">
           duo
@@ -38,7 +88,7 @@ function App() {
         está aqui.
       </h1>
 
-      <div className="grid grid-cols-6 gap-6 mt-16">
+      <div className="keen-slider mt-12 sm:mt-16" ref={sliderRef}>
         {games.map((game) => (
           <GameBanner
             key={game.id}
--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -6,8 +6,8 @@ interface GameBannerProps {
 
 const GameBanner = ({ bannerUrl, title, adsCount }: GameBannerProps) => {
   return (
-    <a href="" className="relative rounded-lg overflow-hidden">
-      <img src={bannerUrl} alt="" />
+    <div className="keen-slider__slide relative rounded-lg overflow-hidden cursor-default">
+      <img src={bannerUrl} alt="" className="w-full" />
 
       <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
         <strong className="font-bold text-white">{title}</strong>
@@ -15,7 +15,7 @@ const GameBanner = ({ bannerUrl, title, adsCount }: GameBannerProps) => {
           {adsCount} anúncio(s)
         </span>
       </div>
-    </a>
+    </div>
   );
 };
 
